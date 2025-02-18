69e9be2d2811bb158b543be111a5afc1cc71b58d
Ada Gottensträter <nunuu@gmx.net>
2025-02-18 14:08:44 +0100

    New video driver bringup 1

--- build_comptest.sh
+++ build_comptest.sh
@@ -1,4 +1,4 @@
 set -e # Enable error reporting
 rm misoyume_lower.binary || true
-../spin2cpp/build/flexspin -2 -E -H 360448 -O1,extrasmall,inline-single,experimental,aggressive-mem --charset=shiftjis -DFF_FS_TINY=1 -DFF_FS_NORTC=1 misoyume_upper.spin2
+../spin2cpp/build/flexspin -2 -E -H 360448 -O1,extrasmall,inline-single,experimental,aggressive-mem,peek-args --fcache=128 --charset=shiftjis -DFF_FS_TINY=1 -DFF_FS_NORTC=1 misoyume_upper.spin2
 ../spin2cpp/build/flexspin -2 -l --compress misoyume_lower.spin2
--- build_gbrk.sh
+++ build_gbrk.sh
@@ -1,2 +1,2 @@
-../spin2cpp/build/flexspin -2 -E -H 360448 -O1,extrasmall,inline-single,experimental,aggressive-mem --charset=shiftjis -DFF_FS_TINY=1 -DFF_FS_NORTC=1 misoyume_upper.spin2
+../spin2cpp/build/flexspin -2 -E -H 360448 -O1,extrasmall,inline-single,experimental,aggressive-mem,peek-args --fcache=128 --charset=shiftjis -DFF_FS_TINY=1 -DFF_FS_NORTC=1 misoyume_upper.spin2
 ../spin2cpp/build/flexspin -2 -l -gbrk --compress misoyume_lower.spin2
